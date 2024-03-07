1f039dc9e6d3f8f58e8110151e0cd4281db0ca15
Boonchai Wedwanitsatheiin <neng87@gmail.com>
2024-03-07 22:07:33 +0700

    update index

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String index(){
-        return "home";
+        return "Hello World Springboot";
     }
 }
